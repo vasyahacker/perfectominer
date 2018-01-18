0409394f0e5569acabfb3761be2665975d8b3e13
vasyahacker <vasya@magicfreedom.com>
2018-01-18 05:22:25 +0000

    fix scripts +x attr

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+. ./install_scripts/.perfectolib.sh
 scripts=./install_scripts/*.sh
 for script in $scripts; do
-  [ -x $script ] && $script &&  chmod -x $script
+  [ -x $script ] && {
+  	yN ans "Run $script?"
+  	[ "$ans" == "y" ] && $script && chmod -x $script
+  }
 done
 #
 sudo sync
