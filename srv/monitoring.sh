77390e525e011f2c66d7e1b9ffeb1b4ab4b5e290
vasyahacker <vasya@magicfreedom.com>
2018-02-05 12:20:20 +0400

    interface fix

--- srv/monitoring.sh
+++ srv/monitoring.sh
@@ -7,5 +7,5 @@ do
   top -bn3 | awk '/^top -/ { p=!p } { if (!p) print }' > $todir/top
   lan="`/srv/work/traffic.sh eth0`"
   wlan="`/srv/work/traffic.sh wlan0`"
-  echo -e "Lan:\n$lan\nWlan:\n$wlan" > $todir/traffic
+  echo -e "LAN:\n$lan\nWLAN:\n$wlan" > $todir/traffic
 done
